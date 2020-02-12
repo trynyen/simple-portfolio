f0a26567c58599f5257648d8167358fe75fe5232
Trinh Nguyen <blossom90vt@hotmail.com>
2020-02-11 22:54:59 -0600

    added project figures

--- assets/js/sidenav.js
+++ assets/js/sidenav.js
@@ -1,7 +1,36 @@
+$(document).ready(function() {
+let sideBarOpen;
+let sideNav = document.getElementById("mySidenav");
+let mainArea = document.querySelector(".container");
+let openbtn = document.querySelector(".openbtn");
+let closebtn = document.querySelector(".closebtn");
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+  sideBarOpen = true;
+  sideNav.style.width = "250px";
+}
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+function closeNav() {
+  sideBarOpen = false;
+  sideNav.style.width = "0";
+}
+
+openbtn.addEventListener("click", function(e){
+  e.preventDefault();
+  console.log("clicked")
+  openNav();
+});
+
+closebtn.addEventListener('click', function(e){
+  e.preventDefault();
+  closeNav();
+});
+
+mainArea.addEventListener('click', function(e){
+  e.preventDefault();
+  if (sideBarOpen == true){
+    closeNav();
   }
+});
+
+})
