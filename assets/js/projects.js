90913c9258b152c8d202f9b4d1b48fce86f457a3
Trinh Nguyen <blossom90vt@hotmail.com>
2020-03-28 11:48:03 -0500

    changed html figure tags to js objects

---
+++ assets/js/projects.js
@@ -0,0 +1,68 @@
+const projects = [
+  {
+    name:"Harry Potter Trivia",
+    heroku:"https://simple-hp-trivia.herokuapp.com/",
+    github:"https://github.com/trynyen/triviagame",
+    img:"../images/HP Trivia.png"
+  },
+  {
+    name:"Eat Da Burger",
+    heroku:"https://eat-da-burger-tn.herokuapp.com/",
+    github:"https://github.com/trynyen/eat-da-burger",
+    img:"assets/images/Friend Finder.png"
+  },
+  {
+    name:"Friend Finder",
+    heroku:"https://friend-finder-tn.herokuapp.com/",
+    github:"https://github.com/trynyen/FriendFinder",
+    img:"assets/images/Friend Finder.png"
+  },
+  {
+    name:"Train Scheduler",
+    heroku:"https://train-scheduler-tn.herokuapp.com/",
+    github:"https://github.com/trynyen/Train-Scheduler",
+    img:"assets/images/Train Scheduler.png"
+  },
+  {
+    name:"Word Guess",
+    heroku:"https://horror-word-guess.herokuapp.com/",
+    github:"https://github.com/trynyen/Word-Guess-Game",
+    img:"assets/images/Word Guess.png"
+  },
+  {
+    name:"",
+    heroku:"",
+    github:"",
+    img:""
+  },
+  {
+    name:"",
+    heroku:"",
+    github:"",
+    img:""
+  },
+  {
+    name:"",
+    heroku:"",
+    github:"",
+    img:""
+  },
+  {
+    name:"",
+    heroku:"",
+    github:"",
+    img:""
+  },
+  {
+    name:"",
+    heroku:"",
+    github:"",
+    img:""
+  },
+  {
+    name:"",
+    heroku:"",
+    github:"",
+    img:""
+  },
+]
